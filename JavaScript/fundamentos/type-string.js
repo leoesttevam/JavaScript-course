e51d104c83274c4ea96d4e0173d62672d8970043
leoesttevam <leonardoestevam17@gmail.com>
2023-08-16 16:57:42 -0300

    Fundamentos JavaScript, apredendo o que é boolean, number e string

---
+++ JavaScript/fundamentos/type-string.js
@@ -0,0 +1,30 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = "senhaSuperSegura456!";
+const StringNumeros = "34537";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Leonardo";
+
+console.log(citacao);
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+// template string OU template literal
+
+// teste
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const cidadeA = "belo horizonte";
+const inputA = "Belo Horizonte";
+
+const inputMinusculo = inputA.toLowerCase();
+
+console.log(cidadeA === inputMinusculo); // true
+
+const senhas = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
