ada31a22aa6baa0a3e36da5e1844bfaefddda4d5
leoesttevam <leonardoestevam17@gmail.com>
2023-08-20 12:40:39 -0300

    Conhecendo arrays em JS

---
+++ JavaScript/arrays/removandoNota.js
@@ -0,0 +1,18 @@
+const notas = [10, 6, 8, 5.5, 10];
+
+// Remove o ultimo elemento
+notas.pop();
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media);
+
+jantarDeHoje = ['🍔', '🌭', '🍕']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')
+
+console.log(jantarDeHoje)
