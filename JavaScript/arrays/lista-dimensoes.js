823cf0230a174adbb558783f823a5655df9f7631
leoesttevam <leonardoestevam17@gmail.com>
2023-08-20 13:01:50 -0300

    Alterando arrays em JS

---
+++ JavaScript/arrays/lista-dimensoes.js
@@ -0,0 +1,14 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const media = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, media];
+
+console.log(
+    `A aluna da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}
+     A nota dessa aluna é ${listaDeAlunosEMedias[1][1]}`
+);
+
+const funcionarios = [
+    ["Ana", "Juliana", "Leonardo"],
+    [30, 35, 28]
+  ];
